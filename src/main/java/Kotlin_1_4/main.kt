2e55f2092f432a6b87935235265c14e09909c20a
mel182 <vrol0004@gmail.com>
2020-10-17 23:56:30 +0200

    coroutines flow exercise added

--- src/main/java/Kotlin_1_4/main.kt
+++ src/main/java/Kotlin_1_4/main.kt
@@ -1,5 +1,40 @@
 package Kotlin_1_4
 
+import kotlin.reflect.KClass
+import kotlin.reflect.full.createInstance
+
 fun main() {
     println("main function called")
+
+    val myModel = foo(Model2::class,"test")
+    val myModel2 = foo(Model3::class,"test")
+
+    println("My model: ${myModel}")
+    println("My model 2: ${myModel2}")
 }
+
+
+data class Model2(val name:String = "", val age:Int = 1) : BaseClass()
+data class Model3(val name:String = "", val age:Int = 1) : BaseClass()
+
+abstract class BaseClass{
+    var test:String = ""
+
+}
+
+inline fun<reified T: BaseClass> foo(type: KClass<T>, name:String): T {
+    val resourceModel = T::class.createInstance() as BaseClass
+    resourceModel.test = name
+
+    return when (type)
+    {
+        BaseClass::class -> resourceModel as T
+        else -> T::class.createInstance()
+    }
+}
+
+
+
+
+
+
---
+++ src/main/java/concurrency/coroutines_flow/main.kt
@@ -0,0 +1,60 @@
+package concurrency.coroutines_flow
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+
+    runBlocking {
+
+        println("Receiving numbers")
+//        sendNumbers().collect {
+//            println("Received number: $it")
+//        }
+//        sendNumbersAsFlow().collect {
+//            println("Received number builder: $it")
+//        }
+
+        sendNumbersAsFlow().map {
+            delay(300)
+            it
+        }
+        .filter { it <= 10 }
+        .onEach {
+
+            println("Value $it")
+            check( it != 7)
+        }
+        .catch { e -> println("Caught exception $e") }
+        .onCompletion { println("Flow completed") }
+        .transform {
+          emit("Emitting string $it")
+        }.collect {
+            println("Received number builder: $it")
+        }
+
+//        flowOfExample().collect {
+//            println("Received number builder: $it")
+//        }
+        println("Finished receiving numbers")
+    }
+}
+
+// This emit number with delay
+fun sendNumbers(): Flow<Int> = flow {
+
+    val primeList = listOf(2,3,5,7,11,13,17,19,23,29)
+    primeList.forEach {
+        delay(it * 100L)
+        emit(it)
+    }
+}
+
+// This is an example using the 'as flow' builder
+fun sendNumbersAsFlow() = listOf(2,3,5,7,11,13,17,19,23,29).asFlow()
+
+// This is an example using the 'flow of' builder
+fun flowOfExample() = flowOf("One","Two","Three")
+
+
