114765a3d0f3f9ba3d998bfb994f24ba45f84d5b
Melchior Vrolijk <vrol0004@gmail.com>
2021-06-06 20:56:41 +0200

    string to boolean strict example added

---
+++ src/main/java/Kotlin_1_5/string_to_boolean_strict.kt
@@ -0,0 +1,26 @@
+package Kotlin_1_5
+
+import java.lang.Exception
+
+// Two new functions introduce case-sensitive strict versions of the existing String?.toBoolean()
+fun main() {
+
+    val stringValue = "true".lowercase()
+
+    // 'String.toBooleanStrict()' throws an exception for all inputs except the literals 'true' and 'false'.
+    // 'String.toBooleanStrictOrNull()' returns null for all inputs except the literals 'true' and 'false'.
+    // Note: It is case sensitive, it must be in de lower case form otherwise it will throw an exception
+
+    println("boolean strict: ${stringValue.toBooleanStrict()}")
+    println("boolean strict or null: ${stringValue.toBooleanStrictOrNull()}")
+
+    try {
+        val stringInvalidBoolean = "Kotlin"
+
+        println("boolean strict or null: ${stringInvalidBoolean.toBooleanStrictOrNull()}")
+        println("boolean strict: ${stringInvalidBoolean.toBooleanStrict()}") // Exception
+    }catch (e:Exception)
+    {
+        println("Kotlin string value exception: ${e.message}")
+    }
+}
