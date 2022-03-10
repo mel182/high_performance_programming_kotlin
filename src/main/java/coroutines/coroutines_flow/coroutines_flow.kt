6753e7b90fef95777bfa6bf6ae154a891514b29d
mel182 <vrol0004@gmail.com>
2022-03-10 22:19:54 +0100

    more coroutine flow example added

--- src/main/java/coroutines/coroutines_flow/coroutines_flow.kt
+++ src/main/java/coroutines/coroutines_flow/coroutines_flow.kt
@@ -1,8 +1,7 @@
 package coroutines.coroutines_flow
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.runBlocking
 
 /**
  * Coroutine flows
@@ -18,7 +17,12 @@ fun main() {
 //        executeEmitNumberExample()
 //        executeFlowBuilderExample()
 //        executeNumberAsFlowExample()
-        executeNumberAsFlowExample2()
+//        executeNumberAsFlowExample2()
+//        countdownTimerExample()
+//        countExample()
+//        reducerExample()
+//        foldExample()
+        flattenFlowExample()
     }
 }
 
@@ -80,6 +84,121 @@ private suspend fun executeNumberAsFlowExample2() {
     println("\n|----------- execute number as flow example 2 ----------|\n")
 }
 //endregion
+
+private suspend fun countdownTimerExample() {
+
+    val countdownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            delay(1000)
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    countdownFlow.collect{ time ->
+        println("collected int: ${time}")
+    }
+
+    println("|-------- collect latest example -------|")
+    // collect latest will collect ONLY the latest in case there is a delay
+    countdownFlow.collectLatest{ time ->
+        delay(1500) // simulate delay, in this case it will delay it
+                             // for 1.5 sec while every 1 sec value is coming
+                             // in.
+        println("collected latest int: ${time}")
+    }
+    println("|-------- collect latest example -------|")
+    println("collected ended")
+}
+
+private suspend fun countExample() {
+
+    val countdownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            delay(1000)
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    val count = countdownFlow.onEach {
+        println("Item found: $it")
+        it
+    }.count { it > 5 }
+    println("Count result: $count")
+}
+
+private suspend fun reducerExample() {
+
+    val countdownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    // Accumulates value starting with the first element and
+    // applying operation to current accumulator (10 in this case) value
+    val reducer = countdownFlow.reduce { accumulator, value ->
+        println("Item found, accumulator: $accumulator and value: $value")
+        accumulator
+    }
+    println("Reducer result: $reducer")
+}
+
+private suspend fun foldExample() {
+
+    val countdownFlow = flow<Int> {
+        val startingValue = 10
+        var currentValue = startingValue
+        emit(startingValue)
+        while (currentValue > 0) {
+            currentValue--
+            emit(currentValue)
+        }
+    }
+
+    // Accumulates value starting with initial value (15 in this case) and applying operation
+    // current accumulator value and each element
+    val reducer = countdownFlow.fold(15) { accumulator, value ->
+        println("Item found, accumulator: $accumulator and value: $value")
+        accumulator
+    }
+    println("Reducer result: $reducer")
+}
+
+private suspend fun flattenFlowExample() {
+
+    val flow1 = flow<Int> {
+        emit(1)
+        delay(500)
+        emit(2)
+    }
+
+    println("|--------- flat map concat ---------|")
+    // This can be used when you want to retrieve data from your local database and
+    // remote server and combine them into one flow
+    flow1.flatMapConcat { value ->
+
+        flow {
+            emit(value + 1)
+            delay(500)
+            emit(value + 2)
+        }
+    }.collect{ value ->
+        println("map concat value: $value")
+    }
+    println("|--------- flat map concat ---------|")
+}
 
 // This emit number with delay
 fun sendNumbers(): Flow<Int> = flow {
