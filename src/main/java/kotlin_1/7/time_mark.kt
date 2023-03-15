c8c04335a22e12b4e646168a28192e52d1050961
mel182 <vrol0004@gmail.com>
2023-03-15 21:23:44 +0100

    time mark example added

---
+++ src/main/java/kotlin_1/7/time_mark.kt
@@ -0,0 +1,21 @@
+@file:OptIn(ExperimentalTime::class)
+
+package kotlin_1.`7`
+
+import kotlin.time.ExperimentalTime
+import kotlin.time.TimeSource
+
+// Kotlin 1.7.0 improves the performance of time measurement functionality
+// by changing the time marks returned by TimeSource.Monotonic into inline
+// value classes.
+// This means that calling functions like markNow(), elapsedNow(), measureTime(),
+// and measureTimedValue() doesn't allocate wrapper classes for their TimeMark instances.
+// Especially when measuring a piece of code that is part of a hot path, this can help
+// minimize the performance impact of the measurement:
+
+fun main() {
+
+    val mark = TimeSource.Monotonic.markNow() // Returned `TimeMark` is inline class
+    val elapsedDuration = mark.elapsedNow()
+    println("elapsed duration: $elapsedDuration")
+}
