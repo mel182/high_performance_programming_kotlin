1c12798928e57b75f7597b292c744e9e8baf83d7
mel182 <vrol0004@gmail.com>
2023-03-15 20:53:57 +0100

    regular expression example added

---
+++ src/main/java/kotlin_1/7/regular_expression_matching.kt
@@ -0,0 +1,26 @@
+package kotlin_1.`7`
+
+// The Regex.matchAt() and Regex.matchesAt() functions, introduced in 1.5.30,
+// are now Stable. They provide a way to check whether a regular expression
+// has an exact match at a particular position in a String or CharSequence.
+
+// matchesAt() checks for a match and returns a boolean result.
+
+fun main() {
+
+    val testText = "Kotlin 1.7.0 is on its way!"
+    // regular expression: one digit, dot, one digit, dot, one or more digits
+    val versionRegex = "\\d[.]\\d[.]\\d".toRegex()
+
+    testText.firstOrNull()?.let { textAtIndex0 ->
+        println("Character at index 0: $textAtIndex0")
+        println(versionRegex.matchesAt(input = testText, index = 0))
+    }
+
+    if (testText.length > 7) {
+        val textAtIndex7 = testText[7]
+        println("Character at index 7: $textAtIndex7")
+        println(versionRegex.matchesAt(input = testText, index = 7))
+    }
+
+}
