190c6852e3f9a8992547bc12a8d92194381ac4c8
mel182 <vrol0004@gmail.com>
2023-03-16 20:18:17 +0100

    work in progress

---
+++ src/main/java/kotlin_1/8/cbrt.kt
@@ -0,0 +1,18 @@
+@file:OptIn(ExperimentalStdlibApi::class)
+
+package kotlin_1.`8`
+
+import kotlin.math.cbrt
+
+//The cbrt() function, which allows you to compute the real cube
+// root of a double or float, is now Stable.
+
+fun main() {
+
+    val num = 27
+    val negativeNum = -num
+
+    println("The cube root of ${num.toDouble()} is ${cbrt(num.toDouble())}") // 3.0
+    println("The cube root of ${negativeNum.toDouble()} is: ${cbrt(negativeNum.toDouble())}") // -3.0
+
+}
